5f882e690cfd88f83d87804675185c41d35c1b8e
Abdallah867 <abdoallahusma5704@gmail.com>
2024-04-25 23:29:34 +0100

    setup architecture

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:go_router/go_router.dart';
+
+abstract class AppRoutes {
+  static final router = GoRouter(
+    routes: [
+      // GoRoute(
+      //   path: '/',
+      //   builder: (context, state) => const HomeView(),
+      // ),
+      // GoRoute(
+      //   path: kLoginView,
+      //   builder: (context, state) => const LoginView(),
+      // ),
+      // GoRoute(
+      //   path: kRegisterView,
+      //   builder: (context, state) => const RegisterView(),
+      // ),
+      // GoRoute(
+      //   path: kHomeView,
+      //   builder: (context, state) => const HomeView(),
+      // ),
+    ],
+  );
+}
---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,5 @@
+abstract class AppRoutes {
+  static const kRegisterView = '/registerView';
+  static const kLoginView = '/loginView';
+  static const kHomeView = '/homeView';
+}
