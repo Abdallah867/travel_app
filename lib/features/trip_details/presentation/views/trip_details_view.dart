097104ccf753ff761a5ae3b82939daac754d6f5e
Abdallah867 <abdoallahusma5704@gmail.com>
2024-05-07 20:59:59 +0200

    Refactor trip details view layout and add custom circular icon
    Refactored the layout structure of the trip details view and added a custom
    circular icon widget for better UI design and functionality.

--- lib/features/trip_details/presentation/views/trip_details_view.dart
+++ lib/features/trip_details/presentation/views/trip_details_view.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:travel_app/core/utils/font_weight_helper.dart';
 import 'package:travel_app/core/utils/text_styles.dart';
@@ -15,78 +13,116 @@ class TripDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(),
-      body: CustomScrollView(
-        slivers: [
-          const SliverAppBar(),
-          SliverToBoxAdapter(
-            child: Container(
-              height: 400.h,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Color(0xFFD9D9D9),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(32.r),
-                    bottomRight: Radius.circular(32.r)),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Stack(
+                children: [
+                  Container(
+                    height: 400.h,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFD9D9D9),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(32.r),
+                          bottomRight: Radius.circular(32.r)),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 16.0.w, vertical: 16.h),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustomCircularIcon(
+                          icon: Icons.arrow_back,
+                        ),
+                        CustomCircularIcon(
+                          icon: Icons.bookmark_outline,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          const SliverToBoxAdapter(child: VerticalSpace(size: 96)),
-          SliverToBoxAdapter(
-            child: Row(
-              children: [
-                HorizontalSpace(size: 24.w),
-                Text(
-                  'Description',
-                  style: TextStyles.textStyle16
-                      .copyWith(fontWeight: FontWeightHelper.bold),
-                ),
-              ],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Row(
-              children: [
-                HorizontalSpace(size: 24.w),
-                Expanded(
-                  child: Text(
-                    'Lorem ipsum jkjkjljjjllkjljljlkjljjkljlkjlkljmmmmmhdjshdjshdjshdjhsjdhsjdhsjdhjsdhmmmmmmdjshjdshjdhsjdhsjds Read More....',
+            const SliverToBoxAdapter(child: VerticalSpace(size: 96)),
+            SliverToBoxAdapter(
+              child: Row(
+                children: [
+                  HorizontalSpace(size: 24.w),
+                  Text(
+                    'Description',
                     style: TextStyles.textStyle16
-                        .copyWith(fontWeight: FontWeightHelper.regular),
+                        .copyWith(fontWeight: FontWeightHelper.bold),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SliverToBoxAdapter(
-            child: Row(
-              children: [
-                HorizontalSpace(size: 24.w),
-                Expanded(
-                  child: Text(
-                    'Lorem ipsum jkjkjljjjllkjljljlkjljjkljlkjlkljmmmmmhdjshdjshdjshdjhsjdhsjdhsjdhjsdhmmmmmmdjshjdshjdhsjdhsjds Read More....',
-                    style: TextStyles.textStyle16
-                        .copyWith(fontWeight: FontWeightHelper.regular),
+            SliverToBoxAdapter(
+              child: Row(
+                children: [
+                  HorizontalSpace(size: 24.w),
+                  Expanded(
+                    child: Text(
+                      'Lorem ipsum jkjkjljjjllkjljljlkjljjkljlkjlkljmmmmmhdjshdjshdjshdjhsjdhsjdhsjdhjsdhmmmmmmdjshjdshjdhsjdhsjds Read More....',
+                      style: TextStyles.textStyle16
+                          .copyWith(fontWeight: FontWeightHelper.regular),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SliverToBoxAdapter(
-            child: Row(
-              children: [
-                HorizontalSpace(size: 24.w),
-                Expanded(
-                  child: Text(
-                    'Lorem ipsum jkjkjljjjllkjljljlkjljjkljlkjlkljmmmmmhdjshdjshdjshdjhsjdhsjdhsjdhjsdhmmmmmmdjshjdshjdhsjdhsjds Read More....',
-                    style: TextStyles.textStyle16
-                        .copyWith(fontWeight: FontWeightHelper.regular),
+            SliverToBoxAdapter(
+              child: Row(
+                children: [
+                  HorizontalSpace(size: 24.w),
+                  Expanded(
+                    child: Text(
+                      'Lorem ipsum jkjkjljjjllkjljljlkjljjkljlkjlkljmmmmmhdjshdjshdjshdjhsjdhsjdhsjdhjsdhmmmmmmdjshjdshjdhsjdhsjds Read More....',
+                      style: TextStyles.textStyle16
+                          .copyWith(fontWeight: FontWeightHelper.regular),
+                    ),
                   ),
-                ),
-                VerticalSpace(size: 1000)
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            SliverToBoxAdapter(
+              child: Row(
+                children: [
+                  HorizontalSpace(size: 24.w),
+                  Expanded(
+                    child: Text(
+                      'Lorem ipsum jkjkjljjjllkjljljlkjljjkljlkjlkljmmmmmhdjshdjshdjshdjhsjdhsjdhsjdhjsdhmmmmmmdjshjdshjdhsjdhsjds Read More....',
+                      style: TextStyles.textStyle16
+                          .copyWith(fontWeight: FontWeightHelper.regular),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCircularIcon extends StatelessWidget {
+  final IconData icon;
+  const CustomCircularIcon({
+    super.key,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 28,
+      child: Icon(
+        icon,
+        size: 32,
       ),
     );
   }
