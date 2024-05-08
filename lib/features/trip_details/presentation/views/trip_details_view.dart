86e82f89c917104f63f8a1e5cfd70169539e8fde
Abdallah867 <abdoallahusma5704@gmail.com>
2024-05-08 19:38:26 +0200

    Add DescriptionWidget to TripDetailsView
    Import and use DescriptionWidget in TripDetailsView

--- lib/features/trip_details/presentation/views/trip_details_view.dart
+++ lib/features/trip_details/presentation/views/trip_details_view.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-import 'package:travel_app/core/utils/font_weight_helper.dart';
-import 'package:travel_app/core/utils/text_styles.dart';
-import 'package:travel_app/core/widgets/horizontal_space.dart';
 import 'package:travel_app/core/widgets/vertical_widget.dart';
 
 import 'widgets/custom_circular_icon.dart';
+import 'widgets/description_widget.dart';
+import 'widgets/save_button.dart';
 
 class TripDetailsView extends StatelessWidget {
   final String tripId;
@@ -15,7 +14,6 @@ class TripDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(),
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
@@ -42,10 +40,7 @@ class TripDetailsView extends StatelessWidget {
                           icon: Icons.arrow_back_ios_new,
                           onPressed: () => context.pop(),
                         ),
-                        CustomCircularIcon(
-                          onPressed: () {},
-                          icon: Icons.bookmark_outline,
-                        ),
+                        const SaveButton(),
                       ],
                     ),
                   ),
@@ -53,60 +48,7 @@ class TripDetailsView extends StatelessWidget {
               ),
             ),
             const SliverToBoxAdapter(child: VerticalSpace(size: 96)),
-            SliverToBoxAdapter(
-              child: Row(
-                children: [
-                  HorizontalSpace(size: 24.w),
-                  Text(
-                    'Description',
-                    style: TextStyles.textStyle16
-                        .copyWith(fontWeight: FontWeightHelper.bold),
-                  ),
-                ],
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Row(
-                children: [
-                  HorizontalSpace(size: 24.w),
-                  Expanded(
-                    child: Text(
-                      'Lorem ipsum jkjkjljjjllkjljljlkjljjkljlkjlkljmmmmmhdjshdjshdjshdjhsjdhsjdhsjdhjsdhmmmmmmdjshjdshjdhsjdhsjds Read More....',
-                      style: TextStyles.textStyle16
-                          .copyWith(fontWeight: FontWeightHelper.regular),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Row(
-                children: [
-                  HorizontalSpace(size: 24.w),
-                  Expanded(
-                    child: Text(
-                      'Lorem ipsum jkjkjljjjllkjljljlkjljjkljlkjlkljmmmmmhdjshdjshdjshdjhsjdhsjdhsjdhjsdhmmmmmmdjshjdshjdhsjdhsjds Read More....',
-                      style: TextStyles.textStyle16
-                          .copyWith(fontWeight: FontWeightHelper.regular),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Row(
-                children: [
-                  HorizontalSpace(size: 24.w),
-                  Expanded(
-                    child: Text(
-                      'Lorem ipsum jkjkjljjjllkjljljlkjljjkljlkjlkljmmmmmhdjshdjshdjshdjhsjdhsjdhsjdhjsdhmmmmmmdjshjdshjdhsjdhsjds Read More....',
-                      style: TextStyles.textStyle16
-                          .copyWith(fontWeight: FontWeightHelper.regular),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            const SliverToBoxAdapter(child: DescriptionWidget()),
           ],
         ),
       ),
