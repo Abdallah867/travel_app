9c1961613e0761320c263b6e808f2f8fa9924fa9
Abdallah867 <abdoallahusma5704@gmail.com>
2024-05-15 00:14:02 +0200

    booking Dialog done

--- lib/features/trip_details/presentation/views/widgets/booking_and_trip_plan_button.dart
+++ lib/features/trip_details/presentation/views/widgets/booking_and_trip_plan_button.dart
@@ -8,6 +8,7 @@ import 'package:travel_app/core/widgets/horizontal_space.dart';
 import 'package:travel_app/features/trip_details/presentation/views/widgets/custom_circular_icon.dart';
 
 import '../../../../../core/utils/app_strings.dart';
+import '../../../../../core/utils/text_styles.dart';
 
 class BookingAndTripPlanButton extends StatelessWidget {
   const BookingAndTripPlanButton({
@@ -31,11 +32,75 @@ class BookingAndTripPlanButton extends StatelessWidget {
             child: CustomButton(
               text: AppStrings.bookNow,
               onPressed: () {
-                context.push(AppRoutes.kBookingView);
+                _dialogBuilder(context);
+                // context.push(AppRoutes.kBookingView);
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+Future<void> _dialogBuilder(BuildContext context) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+          'Details de Reservation',
+          style: TextStyles.textStyle20,
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Nbr d\'Adultes',
+                  style: TextStyles.textStyle14,
+                ),
+                const SmallButton(),
+              ],
+            ),
+          ],
+        ),
+        actions: [
+          CustomButton(
+            text: 'Continue',
+            onPressed: () {},
+          ),
+        ],
+      );
+    },
+  );
+}
+
+class SmallButton extends StatelessWidget {
+  const SmallButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: AppColors.secondaryColor,
+          width: 2.0,
+        ),
+        color: AppColors.whiteColor,
+        borderRadius: BorderRadius.circular(5.r),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: Icon(
+          Icons.add,
+          color: AppColors.secondaryColor,
+          size: 20.sp,
+        ),
       ),
     );
   }
