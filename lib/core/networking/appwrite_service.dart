07727b5c79eb5f083a9f2e8608e72633e4782eac
Abdallah867 <abdoallahusma5704@gmail.com>
2024-05-18 12:21:16 +0200

    appwrite service done

--- lib/core/networking/appwrite_service.dart
+++ lib/core/networking/appwrite_service.dart
@@ -3,40 +3,37 @@ import 'package:appwrite/models.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 
+import '../constants/app_constants.dart';
 import '../constants/appwrite_constants.dart';
 import '../errors/failure.dart';
 import '../services/service_locator.dart';
 
 abstract class DatabaseService {
-  Future<Either<Map<String, dynamic>, Failure>> get(
-      {required String id, required String endpoint});
-  Future<Either<void, Failure>> create(
-      {required Map<String, dynamic> data, required String endpoint});
+  Future<Either<Map<String, dynamic>, Failure>> get({
+    required String id,
+    required String endpoint,
+  });
+
+  Future<Either<void, Failure>> create({
+    required Map<String, dynamic> data,
+    required String endpoint,
+  });
+
+  Future<Either<Map<String, dynamic>, Failure>> delete({
+    required String id,
+    required String endpoint,
+  });
+
+  Future<Either<Map<String, dynamic>, Failure>> update({
+    required String id,
+    required String endpoint,
+    required Map<dynamic, dynamic> data,
+  });
 }
 
-final List<Map<String, String>> collections = [
-  {
-    'endpoint': 'profiles',
-    'databaseId': AppwriteConstants.databaseId,
-    'collectionId': AppwriteConstants.profilesCollectionId,
-  },
-];
-
-final List<AppwriteService> collectionInfo = collections.map((collection) {
-  return AppwriteService(
-    database: getIt.get<Databases>(),
-    collectionInfo: collection,
-  );
-}).toList();
-
 class AppwriteService implements DatabaseService {
-  final Databases database;
-  final Map<String, String> collectionInfo;
-
-  const AppwriteService({
-    required this.database,
-    required this.collectionInfo,
-  });
+  final Databases database = getIt.get<Databases>();
+  final String databaseId = AppwriteConstants.databaseId;
 
   @override
   Future<Either<Map<String, dynamic>, Failure>> get({
@@ -45,8 +42,8 @@ class AppwriteService implements DatabaseService {
   }) async {
     try {
       Document response = await database.getDocument(
-        databaseId: AppwriteConstants.databaseId,
-        collectionId: AppwriteConstants.profilesCollectionId,
+        databaseId: databaseId,
+        collectionId: AppConstants.appwriteCollections[endpoint]!,
         documentId: id,
       );
       return left(response.data);
@@ -63,17 +60,65 @@ class AppwriteService implements DatabaseService {
 
   @override
   Future<Either<void, Failure>> create({
-    required Map<dynamic, dynamic> data,
+    required Map<String, dynamic> data,
     required String endpoint,
   }) async {
     try {
       await database.createDocument(
-        databaseId: AppwriteConstants.databaseId,
-        collectionId: AppwriteConstants.profilesCollectionId,
+        databaseId: databaseId,
+        collectionId: AppConstants.appwriteCollections[endpoint]!,
         documentId: UniqueKey().toString(),
         data: data,
       );
       return left(null);
+    } on AppwriteException catch (e) {
+      return right(
+        Failure(errMessage: e.message ?? 'Some unexpected error occurred'),
+      );
+    } catch (e) {
+      return right(
+        Failure(errMessage: e.toString()),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Map<String, dynamic>, Failure>> delete({
+    required String id,
+    required String endpoint,
+  }) async {
+    try {
+      Document response = await database.deleteDocument(
+        databaseId: databaseId,
+        collectionId: AppConstants.appwriteCollections[endpoint]!,
+        documentId: id,
+      );
+      return left(response.data);
+    } on AppwriteException catch (e) {
+      return right(
+        Failure(errMessage: e.message ?? 'Some unexpected error occurred'),
+      );
+    } catch (e) {
+      return right(
+        Failure(errMessage: e.toString()),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Map<String, dynamic>, Failure>> update({
+    required String id,
+    required String endpoint,
+    required Map<dynamic, dynamic> data,
+  }) async {
+    try {
+      Document response = await database.updateDocument(
+        databaseId: databaseId,
+        collectionId: AppConstants.appwriteCollections[endpoint]!,
+        documentId: id,
+        data: data,
+      );
+      return left(response.data);
     } on AppwriteException catch (e) {
       return right(
         Failure(errMessage: e.message ?? 'Some unexpected error occurred'),
