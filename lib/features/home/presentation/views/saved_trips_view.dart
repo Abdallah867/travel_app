0c0cc5d5518a1b3fe5f3e5938ce9f78fc25fa33b
Abdallah867 <abdoallahusma5704@gmail.com>
2024-06-07 16:24:28 +0200

    savedTrips card done

--- lib/features/home/presentation/views/saved_trips_view.dart
+++ lib/features/home/presentation/views/saved_trips_view.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../core/routes/app_routes.dart';
 import '../../../../core/services/service_locator.dart';
+import '../../../../core/utils/app_colors.dart';
+import '../../../../core/utils/text_styles.dart';
 import '../../../../core/widgets/horizontal_space.dart';
+import '../../../../core/widgets/vertical_widget.dart';
 import '../../../auth/presentation/manager/current_account_cubit/current_account_cubit.dart';
 import '../../../trip/data/models/trip_model.dart';
 import '../../../trip/data/repos/trip_repo_impl.dart';
 import '../../../trip/presentation/manager/get_saved_trips_cubit.dart/get_saved_trips_cubit.dart';
 import '../../../trip/presentation/manager/trip_details_cubit/trip_cubit.dart';
-import 'widgets/trip_card.dart';
+import 'widgets/location_and_price_widget.dart';
+import 'widgets/review_widget.dart';
 
 class SavedTripsView extends StatelessWidget {
   const SavedTripsView({super.key});
@@ -61,22 +67,78 @@ class SaveTripListView extends StatelessWidget {
 
     if (savedTrips.isNotEmpty) {
       return SizedBox(
-        height: 300.h,
+        // height: 300.h,
         child: ListView.separated(
-            padding: EdgeInsets.only(left: 16.0.w),
-            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.symmetric(horizontal: 16.0.w),
+            // scrollDirection: Axis.horizontal,
             itemBuilder: (BuildContext context, int index) => BlocProvider(
                   create: (context) => TripCubit(
                     trip: savedTrips[index],
                   ),
-                  child: const TripCard(),
+                  child: const HorizontalTripCard(),
                 ),
             itemCount: savedTrips.length,
             separatorBuilder: (BuildContext context, int index) =>
                 const HorizontalSpace(size: 15)),
       );
     } else {
-      return const Text('No Saved Trips');
+      return const Center(child: Text('No Saved Trips'));
     }
+  }
+}
+
+class HorizontalTripCard extends StatelessWidget {
+  const HorizontalTripCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final trip = BlocProvider.of<TripCubit>(context).trip;
+    return GestureDetector(
+      onTap: () {
+        context.push(
+          '${AppRoutes.kTrips}/1',
+          extra: BlocProvider.of<TripCubit>(context),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          // border radius
+          borderRadius: BorderRadius.circular(16.r),
+          color: AppColors.whiteColor,
+        ),
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 20.w, bottom: 16.h, top: 16.h),
+              height: 72.h,
+              width: 72.w,
+              decoration: BoxDecoration(
+                color: AppColors.greyColor,
+                borderRadius: BorderRadius.all(Radius.circular(16.r)),
+              ),
+            ),
+            const HorizontalSpace(size: 12),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 16.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const HorizontalSpace(size: double.infinity),
+                    Text(trip.title, style: TextStyles.textStyle20Bold),
+                    const VerticalSpace(size: 4),
+                    ReviewWidget(
+                      review: trip.rating,
+                    ),
+                    const VerticalSpace(size: 4),
+                    const LocationAndPriceWidget(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
