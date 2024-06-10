6569d69a1e67863ea8dd816b4f7d8441b1f5afb2
Abdallah867 <abdoallahusma5704@gmail.com>
2024-06-10 11:29:42 +0200

    shimmer for trips list view done

---
+++ lib/features/trip/presentation/views/widgets/save_trip_list_view_shimmer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../../core/widgets/vertical_widget.dart';
+import 'shimmer_trip_card.dart';
+
+class SaveTripListViewShimmer extends StatelessWidget {
+  const SaveTripListViewShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 24.h),
+      // scrollDirection: Axis.horizontal,
+      itemBuilder: (BuildContext context, int index) => const ShimmerTripCard(),
+      itemCount: 5,
+      separatorBuilder: (BuildContext context, int index) =>
+          const VerticalSpace(size: 16),
+    );
+  }
+}
--- lib/features/trip/presentation/views/widgets/saved_trips_bloc_builder.dart
+++ lib/features/trip/presentation/views/widgets/saved_trips_bloc_builder.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../manager/get_saved_trips_cubit.dart/get_saved_trips_cubit.dart';
 import 'save_trip_list_view.dart';
+import 'save_trip_list_view_shimmer.dart';
 
 class SavedTripsBlocBuilder extends StatelessWidget {
   const SavedTripsBlocBuilder({
@@ -13,7 +14,7 @@ class SavedTripsBlocBuilder extends StatelessWidget {
     return BlocBuilder<GetSavedTripsCubit, GetSavedTripsState>(
       builder: (context, state) {
         if (state is GetSavedTripsLoadInProgress) {
-          return const Center(child: CircularProgressIndicator());
+          return const Center(child: SaveTripListViewShimmer());
         } else if (state is GetSavedTripsSuccess) {
           return const Center(child: SaveTripListView());
         } else {
