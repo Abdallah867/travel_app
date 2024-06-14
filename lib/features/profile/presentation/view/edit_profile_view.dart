8a405fde582ec9f1c77cd2a5a0a7d9daceb2d1e5
Abdallah867 <abdoallahusma5704@gmail.com>
2024-06-14 18:31:55 +0200

    adding update data with onlz the changed value

--- lib/features/profile/presentation/view/edit_profile_view.dart
+++ lib/features/profile/presentation/view/edit_profile_view.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
-import '../../../../core/functions/show_snack_bar.dart';
-import '../../../../core/services/service_locator.dart';
-import '../../../../core/utils/app_colors.dart';
-import '../../../../core/utils/app_strings.dart';
-import '../../data/repos/user_profile_repo_impl.dart';
 
 import '../../../../core/functions/custom_app_bar.dart';
-import '../../../../core/widgets/custom_button.dart';
-import '../../../../core/widgets/custom_text_form_field.dart';
-import '../../../../core/widgets/vertical_widget.dart';
+import '../../../../core/services/service_locator.dart';
 import '../../../auth/presentation/manager/current_account_cubit/current_account_cubit.dart';
 import '../../data/models/user_model.dart';
+import '../../data/repos/user_profile_repo_impl.dart';
 import '../manager/cubit/edit_profile_cubit.dart';
-import 'widgets/profile_informations_widget.dart';
+import 'widgets/edit_profile_bloc_consumer.dart';
 
 class EditProfileView extends StatelessWidget {
   const EditProfileView({super.key});
@@ -30,85 +22,11 @@ class EditProfileView extends StatelessWidget {
         create: (context) => EditProfileCubit(
           userProfileRepo: getIt.get<UserProfileRepoImpl>(),
           user: user,
-        )..setInitialValue(),
+        )
+          ..setInitialValue()
+          ..setupControllerListeners(),
         child: EditProfileBlocConsumer(user: user),
       ),
-    );
-  }
-}
-
-class EditProfileBlocConsumer extends StatelessWidget {
-  const EditProfileBlocConsumer({
-    super.key,
-    required this.user,
-  });
-
-  final UserModel user;
-
-  @override
-  Widget build(BuildContext context) {
-    final EditProfileCubit editProfileCubit =
-        BlocProvider.of<EditProfileCubit>(context);
-    return BlocConsumer<EditProfileCubit, EditProfileState>(
-      listener: (context, state) {
-        if (state is EditProfileSuccess) {
-          BlocProvider.of<CurrentAccountCubit>(context)
-              .updateUserInformations(editProfileCubit.user);
-          editProfileCubit.setInitialValue();
-          showSnackBar(
-              context, 'Profile updated successfully', AppStrings.success);
-          context.pop();
-        }
-        if (state is EditProfileFailure) {
-          showSnackBar(context, state.errMessage, AppStrings.error);
-        }
-      },
-      builder: (context, state) {
-        return Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.w),
-            child: Column(
-              children: [
-                const VerticalSpace(size: 36),
-                ProfileInformationsWidget(
-                  user: editProfileCubit.user,
-                ),
-                const VerticalSpace(size: 24),
-                CustomTextFormField(
-                  name: 'Username',
-                  controller: editProfileCubit.usernameController,
-                ),
-                const VerticalSpace(size: 16),
-                CustomTextFormField(
-                  name: 'Email',
-                  controller: editProfileCubit.emailController,
-                ),
-                const VerticalSpace(size: 16),
-                CustomTextFormField(
-                  name: 'Phone Number',
-                  controller: editProfileCubit.phoneNumberController,
-                ),
-                const VerticalSpace(size: 16),
-                CustomTextFormField(
-                  name: 'Password',
-                  isPassword: true,
-                  controller: editProfileCubit.passwordController,
-                ),
-                const VerticalSpace(size: 32),
-                state is! EditProfileLoadInProgress
-                    ? CustomButton(
-                        text: 'Save',
-                        onPressed: () async {
-                          await editProfileCubit.updateUserData();
-                        },
-                        width: double.infinity,
-                      )
-                    : const CircularProgressIndicator(),
-              ],
-            ),
-          ),
-        );
-      },
     );
   }
 }
