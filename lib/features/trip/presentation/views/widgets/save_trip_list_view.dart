7c165221041cf071999cc7987ffc6a53c00da523
Abdallah867 <abdoallahusma5704@gmail.com>
2024-06-24 15:47:29 +0200

    save trips ui refactor considering scroll view

--- lib/features/trip/presentation/views/widgets/save_trip_list_view.dart
+++ lib/features/trip/presentation/views/widgets/save_trip_list_view.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/widgets/sliver_text.dart';
 import '../../../data/models/trip_model.dart';
 import '../../manager/saved_trips_cubit.dart/saved_trips_cubit.dart';
 import 'trips_list.dart';
 
-class SaveTripListView extends StatelessWidget {
-  const SaveTripListView({super.key});
+class SaveTripList extends StatelessWidget {
+  const SaveTripList({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,12 @@ class SaveTripListView extends StatelessWidget {
     if (savedTrips.isNotEmpty) {
       return TripsList(trips: savedTrips);
     } else {
-      return const Center(child: Text('No Saved Trips'));
+      return const SliverFillRemaining(
+        hasScrollBody: false,
+        child: Center(
+          child: Text('No Saved Trips'),
+        ),
+      );
     }
   }
 }
