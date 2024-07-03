792c46cdeced5253bb7ed9590d7df6c7d94df135
Abdallah867 <abdoallahusma5704@gmail.com>
2024-07-03 02:09:56 +0200

    reservation repo done

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -5,10 +5,19 @@ class AppConstants {
   static const String savesCollectionEndpoint = 'saves';
   static const String tripsCollectionEndpoint = 'trips';
   static const String agenciesCollectionEndpoint = 'agencies';
+  static const String travelersCollectionEndpoint = 'travelers';
+  static const String reservationsCollectionEndpoint = 'reservations';
+  static const String payementsCollectionEndpoint = 'payements';
+  static const String tripScheduleCollectionEndpoint = 'tripSchedule';
+
   static const Map<String, String> appwriteCollections = {
     profilesCollectionEndpoint: '662b840e9331aff1d019',
     savesCollectionEndpoint: '66575dd000095253e61f',
     tripsCollectionEndpoint: '6654b7b100027501e8dc',
     agenciesCollectionEndpoint: '66575ca500179e6a8ee3',
+    travelersCollectionEndpoint: '667eb4890027f48f402b',
+    reservationsCollectionEndpoint: '667ec1e0000db20a2ab7',
+    payementsCollectionEndpoint: '667ec1f1003177117283',
+    tripScheduleCollectionEndpoint: '668454dd00240940a70b',
   };
 }
