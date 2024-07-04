7fc3bede52aeef1e56e1168cab663ded57c2a5d7
Abdalla867 <abdoallahusma5704@gmail.com>
2024-07-04 01:08:59 +0100

    refactor folders

--- lib/features/home/presentation/views/widgets/nav_bar_view.dart
+++ lib/features/home/presentation/views/widgets/nav_bar_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../core/services/service_locator.dart';
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../generated/l10n.dart';
-import '../../../../booking/presentation/views/booking_view.dart';
 import '../../../../profile/presentation/view/profile_view.dart';
 import '../../../../trip/data/repos/trip_repo_impl.dart';
 import '../../../../trip/presentation/views/saved_trips_view.dart';
