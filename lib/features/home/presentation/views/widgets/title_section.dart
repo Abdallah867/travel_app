4b911d7a642999167869c29e7975705868934a4f
Abdallah867 <abdoallahusma5704@gmail.com>
2024-07-10 00:32:28 +0200

    date select widget refactor

--- lib/features/home/presentation/views/widgets/title_section.dart
+++ lib/features/home/presentation/views/widgets/title_section.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/text_styles.dart';
-import '../../../../../generated/l10n.dart';
 
 class TitleSection extends StatelessWidget {
   final String title;
+  final Widget? actionTitle;
+  final VoidCallback onTap;
   const TitleSection({
     super.key,
     required this.title,
+    this.actionTitle,
+    required this.onTap,
   });
 
   @override
@@ -21,11 +23,7 @@ class TitleSection extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(title, style: TextStyles.textStyle20),
-          Text(
-            S.of(context).seeAll,
-            style: TextStyles.textStyle14
-                .copyWith(color: AppColors.secondaryColor),
-          ),
+          GestureDetector(onTap: onTap, child: actionTitle),
         ],
       ),
     );
--- lib/features/reservation/presentation/views/widgets/reservation_form.dart
+++ lib/features/reservation/presentation/views/widgets/reservation_form.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/text_styles.dart';
 import '../../../../../core/widgets/custom_text_form_field.dart';
+import '../../../../../core/widgets/horizontal_space.dart';
 import '../../../../../core/widgets/vertical_widget.dart';
+import '../../../../home/presentation/views/widgets/title_section.dart';
 import '../../manager/cubit/reservation_cubit.dart';
 import 'date_select_list_view.dart';
 
@@ -23,6 +28,23 @@ class ReservationForm extends StatelessWidget {
                   state is ReservationLoadInProgress
                       ? const LinearProgressIndicator()
                       : const DateSelectListView(),
+                  const VerticalSpace(size: 16),
+                  TitleSection(
+                    onTap: () {},
+                    title: 'Travelers',
+                    actionTitle: Row(
+                      children: [
+                        const Icon(Icons.add,
+                            color: AppColors.secondaryColor, size: 16),
+                        HorizontalSpace(size: 4.w),
+                        Text(
+                          'Add Travelers',
+                          style: TextStyles.textStyle16
+                              .copyWith(color: AppColors.secondaryColor),
+                        ),
+                      ],
+                    ),
+                  ),
                   const VerticalSpace(size: 16),
                   CustomTextFormField(
                     name: 'Nom',
