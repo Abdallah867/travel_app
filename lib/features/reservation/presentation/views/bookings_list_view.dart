94e5bb97cd7b783ab7ef5ae9331bdc901b6bd674
Abdalla867 <abdoallahusma5704@gmail.com>
2024-09-25 02:28:07 +0100

    booking list view done

--- lib/features/reservation/presentation/views/bookings_list_view.dart
+++ lib/features/reservation/presentation/views/bookings_list_view.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../core/functions/custom_app_bar.dart';
 import '../../../../core/utils/app_colors.dart';
+import '../../../../core/utils/font_weight_helper.dart';
 import '../../../../core/utils/text_styles.dart';
 import '../../../../core/widgets/custom_button.dart';
 import '../../../../core/widgets/horizontal_space.dart';
@@ -17,14 +19,26 @@ class BookingsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [ResCard()],
+    return Scaffold(
+      appBar: customAppBar('Bookings'),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            ReservationCard(),
+            VerticalSpace(size: 16),
+            ReservationCard(),
+            VerticalSpace(size: 16),
+            ReservationCard(),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class ResCard extends StatelessWidget {
-  const ResCard({super.key});
+class ReservationCard extends StatelessWidget {
+  const ReservationCard({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -64,12 +78,16 @@ class ResCard extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const HorizontalSpace(size: double.infinity),
-                      Text('trip.title', style: TextStyles.textStyle16SemiBold),
-                      const VerticalSpace(size: 4),
-                      const ReviewWidget(
-                        review: 5.0,
+                      Text(
+                        'Voyage A Collo Skikda sasasasasasasasas',
+                        style: TextStyles.textStyle16SemiBold,
+                        overflow: TextOverflow.ellipsis,
                       ),
                       const VerticalSpace(size: 4),
+                      Text('Aug 18, 9:00 AM', style: TextStyles.textStyle14),
+                      const VerticalSpace(size: 12),
+                      Text('4 Persons', style: TextStyles.textStyle14SemiBold),
+
                       // const LocationAndPriceWidget(),
                     ],
                   ),
@@ -77,80 +95,40 @@ class ResCard extends StatelessWidget {
               )
             ],
           ),
-          Row(
-            children: [
-              Expanded(
-                child: CustomButton(
-                  text: 'View Details',
-                  onPressed: () {},
+          Padding(
+            padding: EdgeInsets.only(left: 20.w, right: 20.w, bottom: 8.h),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: CustomButton(
+                    height: 40.h,
+                    text: 'View Details',
+                    style: TextStyles.textStyle14.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeightHelper.semiBold),
+                    onPressed: () {},
+                  ),
                 ),
-              ),
-              Expanded(
-                child: CustomButton(
-                  text: 'View Details',
-                  onPressed: () {},
+                const HorizontalSpace(size: 16),
+                Expanded(
+                  child: CustomButton(
+                    color: AppColors.whiteColor,
+                    style: TextStyles.textStyle14.copyWith(
+                      color: Colors.black.withOpacity(.35),
+                      fontWeight: FontWeightHelper.semiBold,
+                    ),
+                    borderSide:
+                        const BorderSide(color: AppColors.inputGrey, width: 2),
+                    height: 40.h,
+                    text: 'View Details',
+                    onPressed: () {},
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           )
         ],
-      ),
-    );
-  }
-}
-
-class ReservationCard extends StatelessWidget {
-  const ReservationCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        // border radius
-        borderRadius: BorderRadius.circular(16.r),
-        boxShadow: const [
-          BoxShadow(
-            color: Color.fromRGBO(17, 17, 26, 0.1),
-            blurRadius: 0,
-            spreadRadius: 0,
-            offset: Offset(
-              0,
-              1,
-            ),
-          ),
-        ],
-        color: AppColors.whiteColor,
-      ),
-      child: Expanded(
-        child: Row(
-          children: [
-            Padding(
-              padding: EdgeInsetsDirectional.only(
-                start: 16.w,
-                top: 12.h,
-                bottom: 12.h,
-              ),
-              child: const CustomImageBox(imageUrl: 'trip.coverImage'),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.w),
-              child: Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const HorizontalSpace(size: double.infinity),
-                    Text('trip.title', style: TextStyles.textStyle16SemiBold),
-                    const VerticalSpace(size: 4),
-                    const ReviewWidget(
-                      review: 5.0,
-                    ),
-                    const VerticalSpace(size: 4),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
       ),
     );
   }
